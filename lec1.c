f7f7febdb729bb9272f5feb52fac8728652f77b1
Synizter <g.pongth@gmail.com>
2017-08-10 01:19:47 +0700

    Add files via upload

---
+++ lec1.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+#include "math.h"
+
+int main(int argc, char* argv[])
+{
+	/* 
+		 ((      /|_/|
+  		  \\.._.'  , ,\  gimme food hoomannn....
+  		  /\ | '.__ v /   
+         (_ .   /   "         
+          ) _)._  _ /    
+           '.\ \|( / ( 
+            '' ''\\ \\
+	THIS IS BLOCK COMMENT
+	*/
+
+	//Print function <- THIS IS LINE COMMENT
+	printf("Hello Class\n");
+
+	return 10;
+}
